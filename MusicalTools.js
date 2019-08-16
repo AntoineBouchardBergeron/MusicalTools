a60b659d91d60a1ea441e9179013fe987a1bb2e3
Antoine B.-B <antoine.bouchard.bergeron@gmail.com>
2019-08-15 21:15:35 -0400

    added a bunch of things

--- MusicalTools.js
+++ MusicalTools.js
@@ -1,17 +1,31 @@
 var StartTime = new Date();
 var BPS = 0.500;
-var playing = true;
+var playing = false;
 var date = new Date();
+var aContext = new AudioContext();
+var beep = aContext.createOscillator();
+
 
 
 function playMetronome(){
+  console.log("fuuuuuuuck");
   while (playing){
     if(StartTime - date.getTime() > BPS){
       t.getTime = StarTime;
+      beep.type = "sawtooth";
+      beep.connect(aContext);
+      beep.start();
+    }
+    else{
+      beep.stop();
     }
     console.log(date.getTime());
   }
 
+}
+
+function ChangeBPS(){
+  BPS = 0.50;
 }
 
 function Play(){
@@ -20,6 +34,7 @@ function Play(){
   } else{
     playing = true;   
     StartTime = date.getTime();   
+    playMetronome();
   }
-  console.log("Play Pressed");
+  console.log("Play Pressed  : " + playing);
 }
